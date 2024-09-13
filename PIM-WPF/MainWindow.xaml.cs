ad2c830bb5d0dddf1c3193122fa9e3a4d22a9ff7
LucasCerione <lucascerione@hotmail.com>
2024-09-12 21:58:26 -0300

    Menu pronto

--- PIM-WPF/MainWindow.xaml.cs
+++ PIM-WPF/MainWindow.xaml.cs
@@ -31,5 +31,50 @@ namespace PIM_WPF
                 this.Close();
             }
         }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // set tooltip visibility
+
+            if(tg_btn.IsChecked == true)
+            {
+
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_Introducao.Visibility = Visibility.Collapsed;
+                tt_Mais.Visibility = Visibility.Collapsed;
+                tt_Obras.Visibility = Visibility.Collapsed;
+                tt_Questionario.Visibility = Visibility.Collapsed;
+                tt_Resultados.Visibility = Visibility.Collapsed;
+
+            }
+            else
+            {
+
+                tt_home.Visibility= Visibility.Visible;
+                tt_Introducao.Visibility = Visibility.Visible;
+                tt_Mais.Visibility = Visibility.Visible;
+                tt_Obras.Visibility = Visibility.Visible;
+                tt_Questionario.Visibility = Visibility.Visible;
+                tt_Resultados.Visibility = Visibility.Visible;
+
+            }
+
+
+        }
+
+        private void tg_btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void tg_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tg_btn.IsChecked = false;
+        }
     }
 }
